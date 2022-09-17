03588097ba805dd80d11173477def8c74b35304a
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2022-09-17 20:52:08 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+var emojiDictionary = {
+  '😊' : 'smiling',
+  '🤨' : 'disbelief',
+  "😥" : 'sad',
+  '😡' : 'angry',
+  '😑' : 'annoyance',
+  '💓' : 'heart',
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
+
 function App() {
+  const [meaning, setMeaning] = useState('');
+
+  function emojiHandler(e) {
+    var userInput = e.target.value;
+
+    var meaning = emojiDictionary[userInput];
+
+    if(meaning === undefined) {
+      meaning = "We don't have this in our database"; 
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Emoji App</h1>
+      <div className='input'><input onChange={emojiHandler} type="text" /></div>
+      <em>{meaning}</em>
+      <p><strong>Emojis we know</strong></p>
+      {emojisWeKnow.map((emoji) => {
+        return (
+          <span onClick={() => {emojiClickHandler(emoji)}} className='emoji' key={emoji}>{emoji}</span>
+        )
+    })}
     </div>
   );
 }
